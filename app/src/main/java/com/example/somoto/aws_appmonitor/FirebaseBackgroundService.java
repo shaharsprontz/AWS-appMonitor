af63ddaf58f40c6a6208a165cf4120b708128295
shaharsprontz <jawa350640>
2017-02-05 19:11:38 +0200

    Added push notification on database change + vibrate

--- app/src/main/java/com/example/somoto/aws_appmonitor/FirebaseBackgroundService.java
+++ app/src/main/java/com/example/somoto/aws_appmonitor/FirebaseBackgroundService.java
@@ -1,9 +1,15 @@
 package com.example.somoto.aws_appmonitor;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -35,6 +41,7 @@ public class FirebaseBackgroundService extends Service {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String text = dataSnapshot.getValue(String.class);
                 Toast.makeText(FirebaseBackgroundService.this, "Database Updated", Toast.LENGTH_LONG).show();
+                createNotification();
             }
 
             @Override
@@ -45,5 +52,41 @@ public class FirebaseBackgroundService extends Service {
         mConditionRef.addValueEventListener(handler);
     }
 
+    private void createNotification(){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setAutoCancel(true)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+        // Get instance of Vibrator from current Context
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+// Vibrate for 300 milliseconds
+        //star wars theme
+        v.vibrate(new long[]{0, 500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500}, -1);
+    // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(this, MainActivity.class);
+
+    // The stack builder object will contain an artificial back stack for the
+    // started Activity.
+    // This ensures that navigating backward from the Activity leads out of
+    // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+    // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(MainActivity.class);
+    // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    // mId allows you to update the notification later on.
+            mNotificationManager.notify(1111, mBuilder.build());
+    }
 
 }
